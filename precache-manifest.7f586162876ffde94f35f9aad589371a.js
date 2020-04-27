082f238521e89c06b9736207b6cf545f2fb0032d
Albert <albertmik1915@gmail.com>
2020-04-27 16:07:41 +0300

    Updates

---
+++ precache-manifest.7f586162876ffde94f35f9aad589371a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e043021d8eebbf56ac93f6a308446a8a",
+    "url": "/mmm/index.html"
+  },
+  {
+    "revision": "7b4d4df101567f4bf004",
+    "url": "/mmm/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3e4c90a20c206f98dc58",
+    "url": "/mmm/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7b4d4df101567f4bf004",
+    "url": "/mmm/static/js/2.afde4a53.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/mmm/static/js/2.afde4a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4c90a20c206f98dc58",
+    "url": "/mmm/static/js/main.5ed6601f.chunk.js"
+  },
+  {
+    "revision": "b0ee5c54abdab9b65d35",
+    "url": "/mmm/static/js/runtime-main.f3ad1897.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/mmm/static/media/persik.4e1ec840.png"
+  }
+]);
